85409c1d8897bbc4221a9e0cfb0a19fe019e3d00
yuvalBerghaus <yuvalBerghaus>
2023-07-11 19:21:57 +0300

    first commit

---
+++ echo-client.py
@@ -0,0 +1,9 @@
+# echo-client.py
+import socket
+HOST = "10.0.2.5" # The server's hostname or IP address
+PORT = 65432 # The port used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+    print(f"Received {data!r}")
